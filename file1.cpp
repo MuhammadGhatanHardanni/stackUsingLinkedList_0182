07cf1e1cfc21a1de63b7e4e9ad452f5a58473dd5
Ghatan55 <ghatanhardanni@gmail.com>
2025-05-22 10:08:14 +0700

    Menambahkan Operasi push untuk memasukkan elemen ke bagian atas tumpukan

--- file1.cpp
+++ file1.cpp
@@ -23,5 +23,15 @@ class Stack {
         top = NULL; // Initialize the stack with a null top pointer
      }
 
+     // Push operation insert an element onto the top of the stack
+     int push(int value){
+        Node* newNode = new Node(); // 1. Allocate memory for the new node
+        newNode->data = value; // 2. assign value
+        newNode->next = top; // 3. Set the next pointer of the new node to the current top
+        top = newNode; // 4. Update the top pointer to the new node
+        cout << "Push Value: " << value << endl;
+        return value;
+     }
+
     
 };
